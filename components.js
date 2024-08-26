5c323141d5cfb996fcf090fbdc30a927451f12db
cramanan <cramananjaonapro@gmail.com>
2024-08-26 11:06:42 +0200

    feat: added XP graph

--- components.js
+++ components.js
@@ -37,37 +37,53 @@ export function Header(user) {
     return header;
 }
 
-export function XPCard({ courses, id }) {
-    if (!courses || !id) return;
+export function XPCard({ id }) {
+    if (!id) return;
     const XP = document.createElement("div");
     const img = document.createElement("img");
     const coursesDiv = document.createElement("div");
 
-    for (const course of courses) {
-        GraphQLQuery({
-            query: 'query rootEventDetails($userId: Int!, $rootEventId: Int!) {\n xp: transaction_aggregate(\n where: {\n userId: { _eq: $userId }\n type: { _eq: "xp" }\n eventId: { _eq: $rootEventId }\n }\n ) { aggregate { sum { amount } } }\n level: transaction(\n limit: 1\n order_by: { amount: desc }\n where: {\n userId: { _eq: $userId }\n type: { _eq: "level" }\n eventId: { _eq: $rootEventId }\n }\n ) { amount }\n }',
-
-            variables: {
-                userId: id,
-                rootEventId: course.id,
-            },
-        }).then((data) => {
-            const subdiv = document.createElement("div");
-            const path = document.createElement("h3");
-            const sum = document.createElement("div");
-
-            subdiv.className = "courses";
-            path.textContent = Title(
-                course.path
-                    .substring(course.path.lastIndexOf("/") + 1)
-                    .replaceAll("-", " ")
-            );
-
-            sum.textContent = formatBytes(data.xp.aggregate.sum.amount);
-            subdiv.append(path, sum);
-            coursesDiv.append(subdiv);
-        });
-    }
+    GraphQLQuery({
+        query: `query courses($userId: Int!) {
+        event(
+            where: {
+            object: { type: { _in: ["module", "piscine"] } }
+            usersRelation : { userId : {_eq : $userId}}
+            }
+            ) {
+                id
+                path
+            }
+        }`,
+        variables: {
+            userId: id,
+        },
+    })
+        .then(({ event }) => {
+            for (const course of event) {
+                GraphQLQuery({
+                    query: 'query rootEventDetails($userId: Int!, $rootEventId: Int!) {\n xp: transaction_aggregate(\n where: {\n userId: { _eq: $userId }\n type: { _eq: "xp" }\n eventId: { _eq: $rootEventId }\n }\n ) { aggregate { sum { amount } } }\n level: transaction(\n limit: 1\n order_by: { amount: desc }\n where: {\n userId: { _eq: $userId }\n type: { _eq: "level" }\n eventId: { _eq: $rootEventId }\n }\n ) { amount }\n }',
+                    variables: {
+                        userId: id,
+                        rootEventId: course.id,
+                    },
+                }).then((data) => {
+                    const subdiv = document.createElement("div");
+                    const path = document.createElement("h3");
+                    const sum = document.createElement("div");
+                    subdiv.className = "courses";
+                    path.textContent = Title(
+                        course.path
+                            .substring(course.path.lastIndexOf("/") + 1)
+                            .replaceAll("-", " ")
+                    );
+                    sum.textContent = formatBytes(data.xp.aggregate.sum.amount);
+                    subdiv.append(path, sum);
+                    coursesDiv.append(subdiv);
+                });
+            }
+        })
+        .catch(console.error);
 
     img.src = "assets/xp.svg";
     img.width = 80;
@@ -165,4 +181,96 @@ export function SkillsGraph({ transactions }) {
     return div;
 }
 
-export function XPGraph() {}
+export function XPGraph() {
+    const div = document.createElement("div");
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const axis = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    const points = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    let sum = 0;
+    const offset = 10;
+    let d = `M${offset} ${100 + offset}`;
+    const xAxis = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "line"
+    );
+
+    const yAxis = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "line"
+    );
+    svg.setAttribute("viewBox", `0 0 ${100 + 2 * offset} ${100 + 2 * offset}`);
+    axis.setAttribute("stroke", "black");
+
+    xAxis.setAttribute("x1", offset);
+    xAxis.setAttribute("x2", 100 + offset);
+    xAxis.setAttribute("y1", 100 + offset);
+    xAxis.setAttribute("y2", 100 + offset);
+    yAxis.setAttribute("y1", offset);
+    yAxis.setAttribute("y2", 100 + offset);
+    yAxis.setAttribute("x1", offset);
+    yAxis.setAttribute("x2", offset);
+
+    path.setAttribute("stroke", "black");
+    path.setAttribute("stroke-width", "0.5");
+
+    path.setAttribute("fill", "none");
+
+    GraphQLQuery({
+        query: `query XPGraph {
+  orderedByDate: transaction(
+    where: {
+      type: { _eq: "xp" }
+      _and: [{ path: { _nilike: "%piscine%" } }, { path: { _nilike: "%checkpoint%" } }]
+    }
+    order_by: { createdAt: asc }
+  ) {
+    id
+    amount
+    createdAt
+  }
+  maxXP: transaction_aggregate(
+    where: {
+      type: { _eq: "xp" }
+      _and: [{ path: { _nilike: "%piscine%" } }, { path: { _nilike: "%checkpoint%" } }]
+    }
+  ) {
+    aggregate {
+      sum {
+        amount
+      }
+    }
+  }
+}`,
+    })
+        .then(({ orderedByDate, maxXP }) => {
+            const maxAmount = maxXP.aggregate.sum.amount;
+            const minDate = Date.parse(orderedByDate[0].createdAt);
+            const maxDate = Date.parse(
+                orderedByDate[orderedByDate.length - 1].createdAt
+            );
+
+            for (const element of orderedByDate) {
+                const dateDiff =
+                    ((Date.parse(element.createdAt) - minDate) /
+                        (maxDate - minDate)) *
+                    100;
+
+                element.amount = sum += element.amount;
+                const XPDiff = (element.amount / maxAmount) * 100;
+                const coords = {
+                    x: 100 - XPDiff + offset,
+                    y: dateDiff + offset,
+                };
+
+                d = `${d} L${coords.y} ${coords.x}`;
+                path.setAttribute("d", d);
+            }
+        })
+        .catch(console.error);
+
+    axis.append(xAxis, yAxis);
+    svg.append(axis, points, path);
+    div.append(svg);
+    return div;
+}
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import {
     Header,
     UserCard,
     XPCard,
-    ProjectsGraph,
+    XPGraph,
 } from "./components.js";
 
 const API_ENDPOINT = "https://zone01normandie.org/api";
@@ -49,12 +49,28 @@ form.addEventListener("submit", (e) => {
 
 async function loadPage(JWT) {
     if (!JWT()) return;
-    const user = {};
-    await GraphQLQuery({ query: "{ user { id login } }" })
-        .then((data) => {
-            user.id = data.user[0].id;
-            user.login = data.user[0].login;
-        })
+    const user = await GraphQLQuery({
+        query: `{
+        user { 
+        id 
+        login 
+        campus 
+        firstName 
+        lastName 
+        auditRatio 
+        totalUp 
+        totalDown 
+        transactions(
+            order_by: [{ type: desc }, { amount: desc }]
+            distinct_on: [type]
+            where: {type: { _like: "skill_%" } }
+            ) {
+            type
+            amount
+            }
+        } }`,
+    })
+        .then(({ user }) => user[0])
         .catch(console.error);
 
     if (!user || !user.id || !user.login) return;
@@ -66,43 +82,17 @@ async function loadPage(JWT) {
     const header = Header(user);
     const main = document.createElement("main");
 
-    const userDiv = document.createElement("div");
+    const userDiv = UserCard(user);
 
-    const XP = document.createElement("div");
-    const audit = document.createElement("div");
+    const XP = XPCard(user);
+    const audit = AuditCard(user);
 
-    const skills = document.createElement("div");
+    const skills = SkillsGraph(user);
 
-    const graph = document.createElement("div");
+    const XPG = XPGraph();
 
-    GraphQLQuery({
-        query: 'query rootEvents($userId: Int!, $campus: String!) {\n event(where: {\n campus: { _eq: $campus }\n usersRelation: { userId: { _eq: $userId} }\n object: { type: { _in: ["module", "piscine"]} }\n }) {\n id\n path\n }\n }',
-        variables: { userId: user.id, campus: "rouen" },
-    })
-        .then(async (data) => {
-            if (!data.event) throw new Error("No event found");
-            Object.assign(user, { courses: data.event });
-
-            XP.replaceWith(XPCard(user));
-            XPGraph(user);
-        })
-        .catch(console.error);
-
-    GraphQLQuery({
-        query: 'query user($userId: Int!) {\n user: user_by_pk(id: $userId) {\n id\n login\n lastName\n firstName\n auditRatio\n totalUp\n totalDown\n transactions (\n order_by: [{ type: desc }, { amount: desc }]\n distinct_on: [type]\n where: { userId: { _eq: $userId }, type: { _like: "skill_%" } },\n )\n { type, amount }\n }\n }',
-        variables: { userId: user.id },
-    })
-        .then((data) => {
-            if (!data.user) throw new Error("No user");
-            Object.assign(user, data.user);
-            skills.replaceWith(SkillsGraph(user));
-            audit.replaceWith(AuditCard(user));
-            userDiv.replaceWith(UserCard(user));
-        })
-        .catch(console.error);
-
+    main.append(XPG, userDiv, XP, audit, skills);
     root.replaceChildren(header, main);
-    main.append(projects, userDiv, XP, audit, skills);
 }
 
 loadPage(getJWT);
